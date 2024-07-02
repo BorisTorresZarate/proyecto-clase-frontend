6eeec74cc6c59998d26163a23e2221e8b6874548
BorisTorres <bogui231297@gmail.com>
2024-07-02 16:40:44 -0500

    primer comiit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  
+  return (
+    <>
+      <div>
+        <p className='text-yellow-200'>sadasdasdasd</p>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
